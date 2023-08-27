c3354e77ee12b64a171dd0ba260ec95926f47824
Daniel Develop <57878266+iath-dev@users.noreply.github.com>
2023-08-27 02:11:21 -0500

    Tests Problem 1 & 2
    Tests of the methods used in the solutions of the first and second problem

--- src/testing.clj
+++ src/testing.clj
@@ -1,6 +1,69 @@
 (ns testing
   (:require [invoice-item]
+            [core]
             [clojure.test :refer :all]))
+
+(deftest val-tax-item
+  (testing "Validate tax item"
+    (testing "True output"
+      (is (->> {:tax/rate 19 :tax/category :iva} (core/val-tax-item) (true?))))
+    (testing "False output"
+      (is (->> {:tax/rate 20 :tax/category :iva} (core/val-tax-item) (false?))))
+      (is (->> {:tax/rate 20 :tax/category :test} (core/val-tax-item) (false?)))
+      (is (->> {:tax/rate 20 :tax/category "iva"} (core/val-tax-item) (false?)))))
+
+(deftest val-ret-item
+  (testing "Validate tax item"
+    (testing "True output"
+      (is (->> {:retention/rate 1 :retention/category :ret_fuente} (core/val-ret-item) (true?))))
+    (testing "False output"
+      (is (->> {:retention/rate 2 :retention/category :ret_fuente} (core/val-ret-item) (false?)))
+      (is (->> {:retention/rate 2 :retention/category :ret} (core/val-ret-item) (false?)))
+      (is (->> {:retention/rate 2 :retention/category "ret_fuente"} (core/val-ret-item) (false?))))))
+
+(def test-duplicate-one {:invoice-item/id          "ii1"
+                         :invoice-item/sku         "SKU 1"
+                         :taxable/taxes            [{:tax/id       "t1"
+                                                     :tax/category :iva
+                                                     :tax/rate     19}]
+                         :retentionable/retentions [{:retention/id       "r1"
+                                                     :retention/category :ret_fuente
+                                                     :retention/rate     1}]})
+(def test-duplicate-two {:invoice-item/id          "ii1"
+                         :invoice-item/sku         "SKU 1"
+                         :taxable/taxes            [{:tax/id       "t1"
+                                                     :tax/category :iva
+                                                     :tax/rate     19}]})
+(def test-duplicate-three {:invoice-item/id          "ii1"
+                         :invoice-item/sku         "SKU 1"
+                         :retentionable/retentions [{:retention/id       "r1"
+                                                     :retention/category :ret_fuente
+                                                     :retention/rate     1}]})
+
+(deftest check-duplicate
+  (testing "Validate duplicate filter"
+    (testing "Invalid out"
+      (is (->> test-duplicate-one (core/check-duplicate) (false?))))
+    (testing "Valid out"
+      (is (->> test-duplicate-two (core/check-duplicate) (true?)))
+      (is (->> test-duplicate-three (core/check-duplicate) (true?))))))
+
+(def filename "invoice.edn")                                ; Invoice EDN filepath
+(def invoice (->> filename (slurp) (clojure.edn/read-string)))    ; Invoice element
+
+(deftest filtrate-item
+  (testing "Method to filtrate valid invoice items"
+    (testing "Valid input"
+      (is (->> invoice (core/filtrate-item) (map :invoice-item/id) (into []) (compare ["ii3" "ii4"]) (= 0))))))
+
+;;; Problem 2
+
+(deftest key-namespace
+  (testing "Valid input"
+    (is (= ":a/b" (core/key-namespace "a" "b")))
+    (is (= ":a/b" (core/key-namespace :a :b)))))
+
+;;; Problem 3
 
 (def invoice-basic {:invoice-item/precise-quantity 10.0,
                     :invoice-item/precise-price 2.0,
@@ -17,7 +80,7 @@
                     :invoice-item/precise-price 2.0,
                     :invoice-item/discount-rate 50.0})
 
-(deftest invoice
+(deftest three
   (testing "Subtotal method inside invoice-item"
     (testing "Empty input"
       (is (thrown? Exception (invoice-item/subtotal {}))))
